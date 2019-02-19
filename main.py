1001fe4d2eab664fa292144f0b45ce3be824d555
henry123nhan <henrynhan1@gmail.com>
2019-02-19 12:36:42 -0600

    updated mineral detector

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import detector
+import minerals
 import random
 import cv2
 import matplotlib.pyplot as plt
@@ -18,9 +18,6 @@ def get_images(dir_path):
 
 
 def main():
-    images = []
-    processor = detector.GoldDetector()
-
     if not os.path.exists(IMG_FOLDER):
         print('Folder \"%s\" is not found. Creating a folder...' % IMG_FOLDER)
         os.makedirs(IMG_FOLDER)
@@ -28,21 +25,21 @@ def main():
         print('Folder \"%s\" is found. Accessing the folder now...' % IMG_FOLDER)
         images = get_images(IMG_FOLDER)
 
-    if len(images) > 0:
-        print("Images found. Randomizing the choice...")
+        if len(images) > 0:
+            print("Images found. Randomizing the choice...")
 
-        # Randomizes the choice of the picture
-        image_path = 'images' + os.sep + images[random.randint(0, len(images)-1)]
-        print('Randomization done. ' + image_path + ' is the image for processing')
+            # Randomizes the choice of the picture
+            image_path = 'images' + os.sep + images[random.randint(0, len(images) - 1)]
+            print('Randomization done. ' + image_path + ' is the image for processing')
 
-        image = cv2.imread(image_path)
-        processed_img = processor.process(image)
+            image = cv2.imread(image_path)
+            result = minerals.process_gold_silver(image)
 
-        # Shows the image on the pyplot
-        plt.imshow(processed_img)
-        plt.show()
-    else:
-        print("No images. Canceling the process...")
+            # Shows the image on the pyplot
+            plt.imshow(result)
+            plt.show()
+        else:
+            print("No images. Canceling the process...")
 
 
 if __name__ == '__main__':
--- minerals.py
+++ minerals.py
@@ -1,8 +1,17 @@
 import numpy as np, cv2
 
+# These are values for assigning minerals based on position
+# According to the rules by FIRST Tech CHallenge game manual for Rover Ruckus,
+# there are minerals for sampling; two are silver while one is gold.
+# In this case for the prototype, assigning the position by a number would
+# tell us which position is gold, silver, or none.
+MINERAL_NONE = 0
+MINERAL_GOLD = 1
+MINERAL_SILVER = 2
+
 # These values are in hsv format
 # EX. [hue, saturation, value]
-LW_GOLD = [17, 100, 80]
+LW_GOLD = [15, 100, 80]
 HI_GOLD = [30, 255, 255]
 
 LW_SILVER = [10, 5, 200]
@@ -15,55 +24,98 @@ GOLD_THRESH = 32
 SILVER_THRESH = 200
 
 
-def process_gold(image):
-    working_image = cv2.resize(image, None, fx=700 / image.shape[0], fy=700 / image.shape[1])
+def inital_process(image, size=(700, 700), b_size=(7, 7)):
+    working_image = cv2.resize(image, None, fx=size[0] / image.shape[0], fy=size[1] / image.shape[1])
+    blurred_image = cv2.GaussianBlur(working_image, b_size, 0)
+    hsv = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2HSV)
+    return working_image, blurred_image, hsv
 
-    blur = cv2.GaussianBlur(working_image, (7, 7), 0)
-    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
+def filter_gold(hsv):
+    """
+    Filters the hsv for gold minerals
+    :param hsv:
+    :return: Mask
+    """
     mask_one = cv2.inRange(hsv, np.array(LW_GOLD), np.array(HI_GOLD))
     mask_two = cv2.inRange(hsv, np.array(LW_BRIGHT), np.array(HI_BRIGHT))
-
-    mask = mask_one + mask_two
-    ret, mask = cv2.threshold(mask, GOLD_THRESH, 128, cv2.THRESH_BINARY)
-
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    closed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    clean = cv2.morphologyEx(closed, cv2.MORPH_OPEN, kernel)
-
-    contours, hierarchy = cv2.findContours(clean, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours]
-
-    if (len(contour_sizes) > 0):
-        biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
-        cv2.drawContours(working_image, [biggest_contour], -1, 255, 5)
-        x, y, w, h = cv2.boundingRect(biggest_contour)
-        cv2.rectangle(working_image, (x, y), (x + w, y + h), [0, 255, 0])
-
-    return working_image
+    return mask_one + mask_two
 
 
-def process_silver(image):
-    working_image = cv2.resize(image, None, fx=700 / image.shape[0], fy=700 / image.shape[1])
-    blurred_image = cv2.GaussianBlur(working_image, (7, 7), 0)
-    hsv = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2HSV)
-
-    # Filter out the color
+def filter_silver(hsv):
+    """
+    Filters the hsv for silver minerals
+    :param hsv:
+    :return: Mask
+    """
     mask_one = cv2.inRange(hsv, np.array(LW_SILVER), np.array(HI_SILVER))
     mask_two = cv2.inRange(hsv, np.array(LW_BRIGHT), np.array(HI_BRIGHT))
-    mask = mask_one + mask_two
+    return mask_one + mask_two
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+
+def clean_mask(mask, shape, size):
+    kernel = cv2.getStructuringElement(shape, size)
     closed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     clean = cv2.morphologyEx(closed, cv2.MORPH_OPEN, kernel)
 
-    contours, hierarchy = cv2.findContours(clean, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours]
+    return clean
 
-    if (len(contour_sizes) > 0):
-        biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
-        cv2.drawContours(working_image, [biggest_contour], -1, 255, 5)
-        x, y, w, h = cv2.boundingRect(biggest_contour)
-        cv2.rectangle(working_image, (x, y), (x + w, y + h), [0, 255, 0])
+
+def find_largest_contour(contours):
+    contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours]
+    biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
+
+    return biggest_contour
+
+
+def draw_contour(image, contour, color):
+    if color is None:
+        color = [0, 255, 0]
+
+    cv2.drawContours(image, [contour], -1, 255, 5)
+    x, y, w, h = cv2.boundingRect(contour)
+    cv2.rectangle(image, (x, y), (x + w, y + h), color)
+
+
+def process_gold_silver(image):
+    """
+    Process the image for one gold mineral and one silver mineral
+    :param image: Image to process
+    :return: image
+    """
+    working_image, blur, hsv = inital_process(image)
+
+    # GOLD
+
+    # Filter the gold and brightness
+    g_mask = filter_gold(hsv)
+
+    # Clean the mask
+    g_clean = clean_mask(g_mask, cv2.MORPH_ELLIPSE, (5, 5))
+
+    # Find the contours in the mask
+    g_contours, g_hierarchy = cv2.findContours(g_clean, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    # SILVER
+
+    # Filter the silver
+    s_mask = filter_silver(hsv)
+
+    # Clean the mask
+    s_clean = clean_mask(s_mask, cv2.MORPH_ELLIPSE, (5, 5))
+
+    # Find the contours in the mask
+    s_contours, s_hierarchy = cv2.findContours(s_clean, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Use largest contours from gold and silver
+    # to draw rectangle over the gold and silver mineral
+    # One for silver, one for gold
+    if len(g_contours) > 0:
+        g_biggest_contour = find_largest_contour(g_contours)
+        draw_contour(working_image, g_biggest_contour, [0, 255, 0])
+
+    if len(s_contours) > 0:
+        s_biggest_contour = find_largest_contour(s_contours)
+        draw_contour(working_image, s_biggest_contour, [0, 0, 255])
 
     return working_image
